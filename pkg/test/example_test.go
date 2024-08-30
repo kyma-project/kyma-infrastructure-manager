43d86956f40fe5c2056220d1028b1d49c03f7d9b
Tobias Schuhmacher <t.schuhmacher@sap.com>
2024-08-30 15:37:47 +0200

    extend test cases

--- pkg/test/example_test.go
+++ pkg/test/example_test.go
@@ -16,7 +16,7 @@ func TestMain(m *testing.M) {
 	ts.Run()
 }
 
-func TestKim(t *testing.T) {
+func TestKCPSystem(t *testing.T) {
 	tc := ts.NewTestCase(t, "Get list of kcp-system pods and check for KIM")
 	tc.Assert("KIM Pod exists", func(t *testing.T, client klient.Client) {
 		var pods v1.PodList
@@ -24,6 +24,17 @@ func TestKim(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Len(t, pods.Items, 1)
 		assert.Contains(t, pods.Items[0].Name, "infrastructure-manager")
+	})
+	tc.Run()
+}
+
+func TestKubeSytem(t *testing.T) {
+	tc := ts.NewTestCase(t, "Get list of kube-system pods")
+	tc.Assert("Kube-system pods exist", func(t *testing.T, client klient.Client) {
+		var pods v1.PodList
+		err := client.Resources("kube-system").List(context.TODO(), &pods)
+		assert.NoError(t, err)
+		assert.True(t, len(pods.Items) > 5)
 	})
 	tc.Run()
 }
