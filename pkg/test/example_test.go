c7c58124ec92f1e084ea204cd9686d3d6ef07332
Tobias Schuhmacher <t.schuhmacher@sap.com>
2024-09-02 10:18:43 +0200

    Add more assert calls

--- pkg/test/example_test.go
+++ pkg/test/example_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
+
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/e2e-framework/klient"
@@ -18,12 +20,23 @@ func TestMain(m *testing.M) {
 
 func TestKCPSystem(t *testing.T) {
 	tc := ts.NewFeature(t, "Get list of kcp-system pods and check for KIM")
+	tc.Assert("KCP-system namespace exists", func(t *testing.T, client klient.Client) {
+		var ns v1.Namespace
+		err := client.Resources(KCPNamespace).Get(context.TODO(), "kcp-system", "", &ns)
+		assert.NoError(t, err)
+		assert.Equal(t, ns.Name, "kcp-system")
+	})
 	tc.Assert("KIM Pod exists", func(t *testing.T, client klient.Client) {
 		var pods v1.PodList
 		err := client.Resources(KCPNamespace).List(context.TODO(), &pods)
 		assert.NoError(t, err)
 		assert.Len(t, pods.Items, 1)
 		assert.Contains(t, pods.Items[0].Name, "infrastructure-manager")
+	})
+	tc.Assert("Compare Shoot-Spec", func(t *testing.T, client klient.Client) {
+		var runtime imv1.Runtime
+		err := client.Resources(KCPNamespace).Create(context.TODO(), &runtime)
+		assert.NoError(t, err)
 	})
 	tc.Run()
 }
