7b53f051389e85471d8cbe4eef1b3d33fb1e6bfa
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-11-18 09:29:32 +0100

    Setting Shoot seedSelector.matchLabels field to the value seed.gardener.cloud/region : <Shoot region>

--- pkg/gardener/shoot/extender/seed_in_shoot_region.go
+++ pkg/gardener/shoot/extender/seed_in_shoot_region.go
@@ -3,12 +3,21 @@ package extender
 import (
 	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func ExtendWithSeedInSameRegion(runtime imv1.Runtime, shoot *gardener.Shoot) error {
 
-	if runtime.Spec.Shoot.EnforceSeedLocation != nil && *runtime.Spec.Shoot.EnforceSeedLocation {
+	if runtime.Spec.Shoot.EnforceSeedLocation != nil && *runtime.Spec.Shoot.EnforceSeedLocation && runtime.Spec.Shoot.Region != "" {
 		//add required label to the shoot
+
+		shoot.Spec.SeedSelector = &gardener.SeedSelector{
+			LabelSelector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"seed.gardener.cloud/region": runtime.Spec.Shoot.Region,
+				},
+			},
+		}
 	}
 
 	return nil
