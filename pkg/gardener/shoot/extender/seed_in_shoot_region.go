5dde5be9c91331731d2b6022976494d2ea154def
Przemysław Golicz <przemyslaw.golicz@sap.com>
2024-11-18 09:39:58 +0100

    Using const for label name and adding extender description

--- pkg/gardener/shoot/extender/seed_in_shoot_region.go
+++ pkg/gardener/shoot/extender/seed_in_shoot_region.go
@@ -6,19 +6,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	seedRegionSelectorLabel = "seed.gardener.cloud/region"
+)
+
+// ExtendWithSeedInSameRegion creates a new extender function that can enforce shoot seed location to be the same as shoot region
+// When EnforceSeedLocation flag in set on RuntimeCR to true it adds special seedSelector field with labelSelector to match seed region with shoot region
 func ExtendWithSeedInSameRegion(runtime imv1.Runtime, shoot *gardener.Shoot) error {
-
 	if runtime.Spec.Shoot.EnforceSeedLocation != nil && *runtime.Spec.Shoot.EnforceSeedLocation && runtime.Spec.Shoot.Region != "" {
-		//add required label to the shoot
-
 		shoot.Spec.SeedSelector = &gardener.SeedSelector{
 			LabelSelector: metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"seed.gardener.cloud/region": runtime.Spec.Shoot.Region,
+					seedRegionSelectorLabel: runtime.Spec.Shoot.Region,
 				},
 			},
 		}
 	}
-
 	return nil
 }
