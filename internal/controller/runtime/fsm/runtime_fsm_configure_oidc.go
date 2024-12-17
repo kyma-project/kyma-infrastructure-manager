658de9e4bd4fc1233869933041909b4771e52432
Michał Drzewiecki <michal.drzewiecki@sap.com>
2024-12-17 15:53:46 +0100

    requeue when oidc cr creation fails

--- internal/controller/runtime/fsm/runtime_fsm_configure_oidc.go
+++ internal/controller/runtime/fsm/runtime_fsm_configure_oidc.go
@@ -47,7 +47,7 @@ func sFnConfigureOidc(ctx context.Context, m *fsm, s *systemState) (stateFn, *ct
 	if err != nil {
 		m.log.Error(err, "Failed to create OpenIDConnect resource")
 		updateConditionFailed(&s.instance)
-		return updateStatusAndStopWithError(err)
+		return requeue()
 	}
 
 	m.log.Info("OIDC has been configured", "Name", s.shoot.Name)
