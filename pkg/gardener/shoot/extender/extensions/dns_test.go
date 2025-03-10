ff4ff12b37faea1474cb1d862d0a76c0fd1daf98
Przemysław Golicz <przemyslaw.golicz@sap.com>
2025-03-10 16:15:54 +0100

    Unit tests development - DNS extension two cases

--- pkg/gardener/shoot/extender/extensions/dns_test.go
+++ pkg/gardener/shoot/extender/extensions/dns_test.go
@@ -1,6 +1,9 @@
 package extensions
 
 import (
+	"encoding/json"
+	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -12,6 +15,7 @@ func TestDNSExtensionsExtender(t *testing.T) {
 		secretName   string
 		prefix       string
 		providerType string
+		expectLocal  bool
 	}{
 		{
 			name:         "Should generate DNS extension for provided external DNS configuration",
@@ -19,18 +23,73 @@ func TestDNSExtensionsExtender(t *testing.T) {
 			secretName:   "aws-route53-secret-dev",
 			prefix:       "dev.kyma.ondemand.com",
 			providerType: "aws-route53",
+			expectLocal:  false,
 		},
 		{
-			name:      "Should generate DNS extension for internal Gardener DNS configuration",
-			shootName: "myshoot",
-			prefix:    "dev.kyma.ondemand.com",
+			name:        "Should generate DNS extension for internal Gardener DNS configuration",
+			shootName:   "myshoot",
+			prefix:      "dev.kyma.ondemand.com",
+			expectLocal: true,
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
-			_, err := NewDNSExtension(testcase.shootName, testcase.secretName, testcase.prefix, testcase.providerType)
+			ext, err := NewDNSExtension(testcase.shootName, testcase.secretName, testcase.prefix, testcase.providerType)
 			require.NoError(t, err)
-
+			if testcase.expectLocal {
+				verifyLocalDNSExtension(t, ext)
+			} else {
+				verifyExternalDNSExtension(t, ext)
+			}
 		})
 	}
+}
 
+func verifyExternalDNSExtension(t *testing.T, ext *gardener.Extension) {
+	require.NotNil(t, ext)
+	require.NotNil(t, ext.ProviderConfig)
+	require.NotNil(t, ext.ProviderConfig.Raw)
+
+	var dnsConfig DNSExtensionProviderConfig
+
+	err := json.Unmarshal(ext.ProviderConfig.Raw, &dnsConfig)
+	require.NoError(t, err)
+	require.NotNil(t, dnsConfig.DNSProviderReplication)
+	require.NotNil(t, dnsConfig.SyncProvidersFromShootSpecDNS)
+
+	assert.Equal(t, "service.dns.extensions.gardener.cloud/v1alpha1", dnsConfig.APIVersion)
+	assert.Equal(t, true, dnsConfig.DNSProviderReplication.Enabled)
+	assert.Equal(t, true, *dnsConfig.SyncProvidersFromShootSpecDNS)
+	assert.Equal(t, "DNSConfig", dnsConfig.Kind)
+
+	require.Len(t, dnsConfig.Providers, 1)
+	provider := dnsConfig.Providers[0]
+
+	require.NotNil(t, provider.Domains)
+	require.NotNil(t, provider.SecretName)
+	require.NotNil(t, provider.Type)
+
+	assert.Equal(t, "aws-route53-secret-dev", *provider.SecretName)
+	assert.Equal(t, "aws-route53", *provider.Type)
+
+	require.Len(t, provider.Domains.Include, 1)
+	assert.Equal(t, "myshoot.dev.kyma.ondemand.com", provider.Domains.Include[0])
+}
+
+func verifyLocalDNSExtension(t *testing.T, ext *gardener.Extension) {
+	require.NotNil(t, ext)
+	require.NotNil(t, ext.ProviderConfig)
+	require.NotNil(t, ext.ProviderConfig.Raw)
+
+	var dnsConfig DNSExtensionProviderConfig
+
+	err := json.Unmarshal(ext.ProviderConfig.Raw, &dnsConfig)
+	require.NoError(t, err)
+	require.Nil(t, dnsConfig.DNSProviderReplication)
+	require.NotNil(t, dnsConfig.SyncProvidersFromShootSpecDNS)
+
+	assert.Equal(t, "service.dns.extensions.gardener.cloud/v1alpha1", dnsConfig.APIVersion)
+	assert.Equal(t, true, *dnsConfig.SyncProvidersFromShootSpecDNS)
+	assert.Equal(t, "DNSConfig", dnsConfig.Kind)
+
+	require.Len(t, dnsConfig.Providers, 0)
 }
