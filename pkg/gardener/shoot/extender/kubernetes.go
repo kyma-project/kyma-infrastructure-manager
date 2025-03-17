f3d2531fba04e4de0172615433b0e8715d99eeda
void404 <wojciech.nawa@sap.com>
2025-03-17 09:22:36 +0100

    Update docs

--- pkg/gardener/shoot/extender/kubernetes.go
+++ pkg/gardener/shoot/extender/kubernetes.go
@@ -10,7 +10,6 @@ import (
 // It sets the Kubernetes version of the Shoot to the version specified in the Runtime.
 // If the version is not specified in the Runtime, it sets the version to the `defaultKubernetesVersion`, set in `converter_config.json`.
 // If the current Kubernetes version on Shoot is greater than the version determined above, it sets the version to the current Kubernetes version.
-// It sets the EnableStaticTokenKubeconfig field of the Shoot to false.
 func NewKubernetesExtender(defaultKubernetesVersion, currentKubernetesVersion string) func(runtime imv1.Runtime, shoot *gardener.Shoot) error {
 	return func(runtime imv1.Runtime, shoot *gardener.Shoot) error {
 		kubernetesVersion := runtime.Spec.Shoot.Kubernetes.Version
