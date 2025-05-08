a05ce60f61547e761dea2cf5097464ce38df3526
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2025-05-08 14:04:34 +0200

    Add enable registry cache property on the Runtime CR

--- api/v1/runtime_types.go
+++ api/v1/runtime_types.go
@@ -126,8 +126,13 @@ type RuntimeList struct {
 
 // RuntimeSpec defines the desired state of Runtime
 type RuntimeSpec struct {
-	Shoot    RuntimeShoot `json:"shoot"`
-	Security Security     `json:"security"`
+	Shoot    RuntimeShoot        `json:"shoot"`
+	Security Security            `json:"security"`
+	Caching  *ImageRegistryCache `json:"imageRegistryCache,omitempty"`
+}
+
+type ImageRegistryCache struct {
+	Enabled bool `json:"enabled"`
 }
 
 // RuntimeStatus defines the observed state of Runtime
