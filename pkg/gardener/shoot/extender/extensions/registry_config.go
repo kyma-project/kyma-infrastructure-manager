7622ba2e91629cbc7143a7f0e25fc31fd2bb99b7
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2025-05-12 13:19:32 +0200

    Added unit test for registry cache extension

--- pkg/gardener/shoot/extender/extensions/registry_config.go
+++ pkg/gardener/shoot/extender/extensions/registry_config.go
@@ -2,8 +2,9 @@ package extensions
 
 import (
 	"encoding/json"
-	registrycache "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3"
+	registrycacheext "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3"
 	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	registrycache "github.com/kyma-project/kim-snatch/api/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/ptr"
@@ -17,12 +18,12 @@ func NewRegistryCacheExtension(cache []registrycache.RegistryCache, enabled bool
 
 func extension(caches []registrycache.RegistryCache, enabled bool) (*gardener.Extension, error) {
 
-	registryConfig := registrycache.RegistryConfig{
+	registryConfig := registrycacheext.RegistryConfig{
 		TypeMeta: v1.TypeMeta{
 			APIVersion: "registry.extensions.gardener.cloud/v1alpha3",
 			Kind:       "RegistryConfig",
 		},
-		Caches: caches,
+		Caches: toRegistryCacheExtension(caches),
 	}
 
 	providerConfigBytes, err := json.Marshal(registryConfig)
@@ -35,6 +36,56 @@ func extension(caches []registrycache.RegistryCache, enabled bool) (*gardener.Ex
 		ProviderConfig: &runtime.RawExtension{
 			Raw: providerConfigBytes,
 		},
-		Disabled: ptr.To(enabled),
+		Disabled: ptr.To(!enabled),
 	}, nil
+}
+
+func toRegistryCacheExtension(cache []registrycache.RegistryCache) []registrycacheext.RegistryCache {
+
+	volumeToCacheExtension := func(volume *registrycache.Volume) *registrycacheext.Volume {
+
+		if volume == nil {
+			return nil
+		}
+
+		return &registrycacheext.Volume{
+			Size:             volume.Size,
+			StorageClassName: volume.StorageClassName,
+		}
+	}
+
+	garbageCollectionExtension := func(garbageCollection *registrycache.GarbageCollection) *registrycacheext.GarbageCollection {
+		if garbageCollection == nil {
+			return nil
+		}
+
+		return &registrycacheext.GarbageCollection{
+			TTL: garbageCollection.TTL,
+		}
+	}
+
+	proxyExtension := func(proxy *registrycache.Proxy) *registrycacheext.Proxy {
+		if proxy == nil {
+			return nil
+		}
+
+		return &registrycacheext.Proxy{
+			HTTPProxy:  proxy.HTTPProxy,
+			HTTPSProxy: proxy.HTTPSProxy,
+		}
+	}
+
+	// Convert the registry cache to the internal format
+	registryCaches := make([]registrycacheext.RegistryCache, 0)
+	for _, c := range cache {
+		registryCaches = append(registryCaches, registrycacheext.RegistryCache{
+			Upstream:            c.Upstream,
+			RemoteURL:           c.RemoteURL,
+			Volume:              volumeToCacheExtension(c.Volume),
+			GarbageCollection:   garbageCollectionExtension(c.GarbageCollection),
+			SecretReferenceName: c.SecretReferenceName,
+			Proxy:               proxyExtension(c.Proxy),
+		})
+	}
+	return registryCaches
 }
---
+++ pkg/gardener/shoot/extender/extensions/registry_config_test.go
@@ -0,0 +1,70 @@
+package extensions
+
+import (
+	registrycacheext "github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3"
+	registrycache "github.com/kyma-project/kim-snatch/api/v1beta1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/yaml"
+	"testing"
+	"time"
+)
+
+func TestNewRegistryCacheExtension(t *testing.T) {
+
+	t.Run("should create registry cache extension", func(t *testing.T) {
+
+		// given
+		caches := []registrycache.RegistryCache{
+			{
+				Upstream: "ghcr.io",
+				GarbageCollection: &registrycache.GarbageCollection{
+					TTL: metav1.Duration{Duration: time.Hour * 24},
+				},
+				SecretReferenceName: ptr.To("secret"),
+				Proxy: &registrycache.Proxy{
+					HTTPSProxy: ptr.To("https://proxy.io:5000"),
+				},
+			},
+			{
+				RemoteURL: ptr.To("http://my-registry.io:5000"),
+				Proxy: &registrycache.Proxy{
+					HTTPProxy: ptr.To("http://proxy.io:5000"),
+				},
+			},
+		}
+
+		// when
+		registryCacheExtension, err := NewRegistryCacheExtension(caches, true)
+
+		// then
+		require.NoError(t, err)
+		require.NotNil(t, registryCacheExtension)
+
+		require.Equal(t, RegistryCacheExtensionType, registryCacheExtension.Type)
+		require.NotNil(t, registryCacheExtension.ProviderConfig)
+		require.NotNil(t, registryCacheExtension.ProviderConfig.Raw)
+		require.Equal(t, registryCacheExtension.Disabled, ptr.To(false))
+
+		var providerConfig registrycacheext.RegistryConfig
+		err = yaml.Unmarshal(registryCacheExtension.ProviderConfig.Raw, &providerConfig)
+		require.NoError(t, err)
+
+		assert.Equal(t, "registry.extensions.gardener.cloud/v1alpha3", providerConfig.TypeMeta.APIVersion)
+		assert.Equal(t, "RegistryConfig", providerConfig.TypeMeta.Kind)
+		assert.Equal(t, "ghcr.io", providerConfig.Caches[0].Upstream)
+		assert.Equal(t, metav1.Duration{Duration: time.Hour * 24}, providerConfig.Caches[0].GarbageCollection.TTL)
+		assert.Equal(t, ptr.To("secret"), providerConfig.Caches[0].SecretReferenceName)
+		assert.Nil(t, providerConfig.Caches[0].Volume)
+		assert.Equal(t, ptr.To("https://proxy.io:5000"), providerConfig.Caches[0].Proxy.HTTPSProxy)
+
+		assert.Equal(t, "http://my-registry.io:5000", *providerConfig.Caches[1].RemoteURL)
+		assert.Equal(t, ptr.To("http://proxy.io:5000"), providerConfig.Caches[1].Proxy.HTTPProxy)
+	})
+
+	t.Run("should create registry cache extension", func(t *testing.T) {
+
+	})
+}
