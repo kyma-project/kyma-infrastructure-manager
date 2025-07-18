c757493df0f4dd8df3685d422d6d3e629e25dd4c
Rafal Foks <r.fok74@gmail.com>
2025-07-18 10:56:40 +0200

    Create an dual-stack configuration for GCP

---
+++ pkg/gardener/shoot/extender/networking/networking.go
@@ -0,0 +1,28 @@
+package networking
+
+import (
+	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
+	hyperscaler2 "github.com/kyma-project/infrastructure-manager/pkg/gardener/shoot/hyperscaler"
+)
+
+func ExtendWithNetworking() func(runtime imv1.Runtime, shoot *gardener.Shoot) error {
+	return func(runtime imv1.Runtime, shoot *gardener.Shoot) error {
+		if isDualStackIPsEnabled(runtime.Spec.Shoot.Networking.DualStack, runtime.Spec.Shoot.Provider.Type) {
+			extendWithDualIPs(shoot)
+		}
+		return nil
+	}
+}
+
+func isDualStackIPsEnabled(dualStack *bool, providerType string) bool {
+	if dualStack == nil {
+		return false
+	}
+	return *dualStack == true && providerType == hyperscaler2.TypeGCP
+}
+
+func extendWithDualIPs(shoot *gardener.Shoot) {
+	shoot.Spec.Networking.IPFamilies = shoot.Spec.Networking.IPFamilies[:0] // reset existing IPFamilies
+	shoot.Spec.Networking.IPFamilies = append(shoot.Spec.Networking.IPFamilies, gardener.IPFamilyIPv4, gardener.IPFamilyIPv6)
+}
