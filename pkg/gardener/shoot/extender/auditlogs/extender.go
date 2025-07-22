22b095a12d23d9abab1bc0a3fa27019a6c01e6ed
m00g3n <marcin.witalis@gmail.com>
2025-07-22 18:02:37 +0200

    Support experimental auditlog policy annotation.

--- pkg/gardener/shoot/extender/auditlogs/extender.go
+++ pkg/gardener/shoot/extender/auditlogs/extender.go
@@ -1,16 +1,25 @@
 package auditlogs
 
 import (
+	"strings"
+
 	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
 )
+
+const experimentalAuditPolicy = "experimental-audit-policy"
 
 type Extend = func(runtime imv1.Runtime, shoot *gardener.Shoot) error
 
 type operation = func(*gardener.Shoot) error
 
 func NewAuditlogExtenderForCreate(policyConfigMapName string, data AuditLogData) Extend {
-	return func(_ imv1.Runtime, shoot *gardener.Shoot) error {
+	return func(rt imv1.Runtime, shoot *gardener.Shoot) error {
+		experimentalAuditPolicyVal, found := rt.Annotations[experimentalAuditPolicy]
+		if found && strings.ToLower(experimentalAuditPolicyVal) == "true" {
+			policyConfigMapName = experimentalAuditPolicy
+		}
+
 		for _, f := range []operation{
 			oSetSecret(data.SecretName),
 			oSetPolicyConfigmap(policyConfigMapName),
