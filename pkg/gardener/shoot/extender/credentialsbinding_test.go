907eb5fb04f27f2530c4282ba91469ff9fc8818c
Michał Drzewiecki <michal.drzewiecki@sap.com>
2025-12-05 08:23:18 +0100

    add tests for credentialbindings extender

---
+++ pkg/gardener/shoot/extender/credentialsbinding_test.go
@@ -0,0 +1,31 @@
+package extender
+
+import (
+	"testing"
+
+	gardener "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtendWithCredentialsBinding_CredentialBindingEnabled(t *testing.T) {
+	runtime := imv1.Runtime{}
+	runtime.Spec.Shoot.SecretBindingName = "my-secret"
+	shoot := &gardener.Shoot{}
+
+	err := ExtendWithCredentialsBinding(true)(runtime, shoot)
+	assert.NoError(t, err)
+	assert.Equal(t, "my-secret", *shoot.Spec.CredentialsBindingName)
+	assert.Nil(t, shoot.Spec.SecretBindingName)
+}
+
+func TestExtendWithCredentialsBinding_CredentialBindingDisabled(t *testing.T) {
+	runtime := imv1.Runtime{}
+	runtime.Spec.Shoot.SecretBindingName = "my-secret"
+	shoot := &gardener.Shoot{}
+
+	err := ExtendWithCredentialsBinding(false)(runtime, shoot)
+	assert.NoError(t, err)
+	assert.Equal(t, "my-secret", *shoot.Spec.SecretBindingName)
+	assert.Nil(t, shoot.Spec.CredentialsBindingName)
+}
