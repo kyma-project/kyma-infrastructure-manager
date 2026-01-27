3bbf4acdf386db634cd437669519d02ddffb2632
Michał Drzewiecki <michal.drzewiecki@sap.com>
2026-01-27 11:45:10 +0100

    allow only one runtimeClient schemes registration

--- pkg/gardener/client.go
+++ pkg/gardener/client.go
@@ -2,19 +2,39 @@ package gardener
 
 import (
 	"fmt"
+	"os"
+	"sync"
+
 	gardeneroidc "github.com/gardener/oidc-webhook-authenticator/apis/authentication/v1alpha1"
 	kyma "github.com/kyma-project/lifecycle-manager/api/v1beta2"
 	registrycacheapi "github.com/kyma-project/registry-cache/api/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/dynamic"
-	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/discovery"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+var (
+	// runtimeScheme is used for runtime cluster clients; initialize once to avoid concurrent AddToScheme calls
+	runtimeScheme     = runtime.NewScheme()
+	runtimeSchemeOnce sync.Once
+)
+
+func initRuntimeScheme() {
+	// register core Kubernetes types and CRDs used by runtime clients
+	utilruntime.Must(clientgoscheme.AddToScheme(runtimeScheme))
+	utilruntime.Must(registrycacheapi.AddToScheme(runtimeScheme))
+	utilruntime.Must(kyma.AddToScheme(runtimeScheme))
+	utilruntime.Must(gardeneroidc.AddToScheme(runtimeScheme))
+	utilruntime.Must(apiextensions.AddToScheme(runtimeScheme))
+}
 
 func NewRestConfigFromFile(kubeconfigFilePath string) (*restclient.Config, error) {
 	rawKubeconfig, err := os.ReadFile(kubeconfigFilePath)
@@ -41,27 +61,10 @@ func GetRuntimeClient(secret corev1.Secret) (client.Client, error) {
 		return nil, err
 	}
 
-	runtimeClient, err := client.New(restConfig, client.Options{})
-	if err != nil {
-		return nil, err
-	}
+	// ensure the shared runtime scheme is initialized exactly once
+	runtimeSchemeOnce.Do(initRuntimeScheme)
 
-	err = registrycacheapi.AddToScheme(runtimeClient.Scheme())
-	if err != nil {
-		return nil, err
-	}
-
-	err = kyma.AddToScheme(runtimeClient.Scheme())
-	if err != nil {
-		return nil, err
-	}
-
-	err = gardeneroidc.AddToScheme(runtimeClient.Scheme())
-	if err != nil {
-		return nil, err
-	}
-
-	err = apiextensions.AddToScheme(runtimeClient.Scheme())
+	runtimeClient, err := client.New(restConfig, client.Options{Scheme: runtimeScheme})
 	if err != nil {
 		return nil, err
 	}
