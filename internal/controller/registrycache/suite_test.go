940cec9ef37dd245ef2e499beb2a75cfeca144c4
Rafal Foks <r.fok74@gmail.com>
2026-05-12 12:54:40 +0200

    Fix unit tests

--- internal/controller/registrycache/suite_test.go
+++ internal/controller/registrycache/suite_test.go
@@ -66,7 +66,7 @@ var _ = BeforeSuite(func() {
 
 	reconciler = NewRegistryCacheConfigReconciler(mgr, logger, fixRuntimeClientGetter(fixRuntimeClients()))
 	Expect(reconciler).NotTo(BeNil())
-	err = reconciler.SetupWithManager(mgr, 1)
+	err = reconciler.SetupWithManager(mgr, 1, ":8082", "infrastructure-manager-registry-cache")
 	Expect(err).To(BeNil())
 
 	go func() {
