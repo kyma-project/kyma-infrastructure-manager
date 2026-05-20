ffd1b6bdf9651b62e6058418d2987f790ef91ccf
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2026-05-20 11:31:10 +0200

    Fix for tests

--- internal/controller/registrycache/suite_test.go
+++ internal/controller/registrycache/suite_test.go
@@ -67,7 +67,7 @@ var _ = BeforeSuite(func() {
 	reconciler = NewRegistryCacheConfigReconciler(mgr, logger, "kcp-system", fixRuntimeClientGetter(fixRuntimeClients()))
 	Expect(reconciler).NotTo(BeNil())
 	suiteCtx, cancelFunc = context.WithCancel(context.Background())
-	err = reconciler.SetupWithManager(suiteCtx, mgr, 1, ":8082", "infrastructure-manager-registry-cache")
+	err = reconciler.SetupWithManager(suiteCtx, mgr, 1, ":0", "infrastructure-manager-registry-cache")
 	Expect(err).To(BeNil())
 
 	go func() {
